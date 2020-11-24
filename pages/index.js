b7ee92901d27e43f7b6b6b5284aab649f28593ae
Jorge Reyes <reyestrevjorge@gmail.com>
2020-11-23 19:15:53 -0600

    feat: Add simple landing
    Add a simple landing to the webpage

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,45 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
 
-export default function Home() {
+const home = () => {
   return (
-    <div className={styles.container}>
+    <>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Adopta, un clic y cambia dos vidas</title>
       </Head>
+      <main>
+        {/* Background */}
+        <div className="background">
+          {/* Banner Blob */}
+          <img src="/Vector1.svg" alt="Blob" className="bannerBlob" />
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+          {/* Photos */}
+          <img
+            src="https://images.unsplash.com/photo-1523626797181-8c5ae80d40c2?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8NDB8fGRvZ3N8ZW58MHx8MHw%3D&auto=format&fit=crop&w=500&q=60"
+            alt="A cute dog"
+            className="photosBlob"
+          />
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          {/* Logo */}
+          <img
+            src="https://adopta.com.mx/static/images/adoptame-logo.png"
+            alt="logo"
+            className="logo"
+          />
         </div>
-      </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
-}
+        {/* Header Section */}
+        <header>
+          <h1>¡Un clic y cambia dos vidas!</h1>
+          <p>
+            ¡Pronto regresaremos más fuertes! <br /> Seguimos activos en
+            nuestras redes sociales @somosadopta.
+          </p>
+        </header>
+      </main>
+    </>
+  );
+};
+
+export default home;
